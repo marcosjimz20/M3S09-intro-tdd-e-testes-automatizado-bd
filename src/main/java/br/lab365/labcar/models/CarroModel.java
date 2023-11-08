c80fe85356ecd12f466d454f4a4811b2d9f267b2
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-11-07 23:52:52 -0300

    feat(container): create Test container

--- src/main/java/br/lab365/labcar/models/CarroModel.java
+++ src/main/java/br/lab365/labcar/models/CarroModel.java
@@ -2,8 +2,6 @@ package br.lab365.labcar.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
